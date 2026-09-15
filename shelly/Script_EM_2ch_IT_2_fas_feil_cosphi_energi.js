598099a47391099198c1ba668a0cecfddc463a8e
Benjamin Svanberg <benjamin.svan@gmail.com>
2026-09-15 19:59:10 +0200

    Update script header with new source URL
    Updated the script header comment to reference the new source URL.

--- shelly/Script_EM_2ch_IT_2_fas_feil_cosphi_energi.js
+++ shelly/Script_EM_2ch_IT_2_fas_feil_cosphi_energi.js
@@ -1,3 +1,5 @@
+// Erstattet av https://github.com/surematu/WebHomeHelpFiles/blob/main/shelly/Script_EM_2ch_IT.js
+
 // Tittel: IT 1 fas energikalk med feil cosphi, 2ch - V1
 // Shelly Pro EM-50
 // Two separate, balanced one-phase loads on a 230 V IT network. Not correct or same voltage reference so it has to assume cosphi, and calculate energy and power.
